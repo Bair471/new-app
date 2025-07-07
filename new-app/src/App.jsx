33ec02ffd0cf4e3b05c0de93e669327a9bd0a9ec
Bair471 <bair.shirnen@gmail.com>
2025-07-07 23:28:15 +0800

    Commit 2 fixes

--- new-app/src/App.jsx
+++ new-app/src/App.jsx
@@ -37,20 +37,26 @@
 
 
 import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from "react";
 
 export default function App() {
+    const [page, setPage] = useState('');
+    const handleClick = function () {
+        setPage = < Link />
+    }
+
     return (
         <div>
             <nav>
                 <ul>
-                    <li><Link to="/">Главная</Link></li>
-                    <li><Link to="/about">О нас</Link></li>
-                    <li><Link to="/contact">Контакты</Link></li>
+                    <Link to="/Home">На главную</Link>
+                    <Link to="/About">О нас</Link>
+                    <Link to="/Contact">Контакты</Link>
                 </ul>
             </nav>
 
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/Home" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
