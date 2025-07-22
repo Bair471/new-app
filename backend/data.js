4fe85f0d70f4e7bd7636492a9aa96e13867c3f1e
Bair471 <bair.shirnen@gmail.com>
2025-07-22 16:38:17 +0800

    Полная перезаливка проекта: frontend и backend

---
+++ backend/data.js
@@ -0,0 +1,6 @@
+export const carsDB = [
+    {id: 1, brand: "Mercedes", model: "s-class"},
+    {id: 2, brand: "Audi", model: "A8"},
+    {id: 3, brand: "Lada", model: "Kalina"}
+]
+
---
+++ backend/server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import cors from 'cors';
+import { carsDB } from './data.js';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/carsDB/:id', (req, res) => {
+  const carsID = parseInt(req.params.id);
+
+  const car = carsDB.find(car => car.id === carsID);
+
+  if (car) {
+    res.json(car);
+  } else {
+    res.status(404).json({ message: 'Машина не найдена' });
+  }
+});
+
+app.get('/carsDB', (req, res) => {
+  res.send(carsDB);
+})
+
+app.post('/carsDB', (req, res) => {
+  const newCar = req.body;
+
+  if (!newCar || !newCar.id || !newCar.brand || !newCar.model) {
+    return res.status(400).json({ message: 'Неверные данные' });
+  }
+
+  carsDB.push(newCar);
+
+  res.status(201).json({ message: 'Машина добавлена', car: newCar });
+})
+
+app.delete('/carsDB/:id', (req, res) => {
+  const idToDelete = parseInt(req.params.id);
+  const index = carsDB.findIndex((car) => car.id === idToDelete);
+
+  if (index === -1) {
+    return res.status(404).json({ message: 'Машина не найдена' });
+  }
+
+  const deletedCar = carsDB.splice(index, 1);
+  res.status(200).json({ message: 'Машина удалена', car: deletedCar[0] });
+});
+
+app.listen(3000, () => {
+  console.log('Сервер работает на http://localhost:3000');
+});
